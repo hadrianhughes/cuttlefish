deeaef04d06bae9c59f031dd347968174ed115d2
Hadrian Hughes <hadrianhughes@gmail.com>
2024-03-11 18:01:08 +0000

    Create basic lexeme and symbol parsers and space consumer

--- compiler/src/Parser.hs
+++ compiler/src/Parser.hs
@@ -1,7 +1,19 @@
 module Parser where
 
-import Text.Megaparsec (Parsec)
-import Data.Text
-import Data.Void
+import           Text.Megaparsec
+import qualified Text.Megaparsec.Char.Lexer as L
+import           Data.Text
+import           Data.Void
 
 type Parser = Parsec Void Text
+
+sc :: Parser ()
+sc = L.space space1 lineCmnt blockCmnt
+  where
+    lineCmnt = L.skipLineComment "//"
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
