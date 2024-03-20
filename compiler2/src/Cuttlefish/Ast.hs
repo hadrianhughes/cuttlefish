e7bfd543e69e34dfcd955d631532b4310c67a654
Hadrian Hughes <hadrianhughes@gmail.com>
2024-03-20 21:16:49 +0000

    Add AST types for type expressions

--- compiler2/src/Cuttlefish/Ast.hs
+++ compiler2/src/Cuttlefish/Ast.hs
@@ -2,11 +2,26 @@ module Cuttlefish.Ast where
 
 import Data.Text (Text)
 
-data FuncDefn = FuncDefn { funcName :: Text
-                         , funcType :: TypeExpr
-                         , funcArgs :: [Bind]
-                         , funcBody :: Expr
-                         , isAlgo   :: Bool }
+data PrimType = Int | Float | Char | Unit deriving Show
+
+data TypeExpr = FuncType    TypeExpr TypeExpr
+              | ListType    TypeExpr
+              | TupleType   [TypeExpr]
+              | StructType  [(Text, TypeExpr)]
+              | SetType     TypeExpr
+              | Constructor [(Text, [TypeExpr])]
+              | TypeVar     Text
+              | PrimType    PrimType
+              deriving Show
+
+data TypeConstraint = TypeConstraint Text Text deriving Show
+
+data FuncDefn = FuncDefn { funcName        :: Text
+                         , funcType        :: TypeExpr
+                         , funcConstraints :: [TypeConstraint]
+                         , funcArgs        :: [Bind]
+                         , funcBody        :: Expr
+                         , funcIsAlgo      :: Bool } deriving Show
 
 data Program = Program
   { funcDefns   :: [FuncDefn]
