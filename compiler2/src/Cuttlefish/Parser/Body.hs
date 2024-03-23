318e5ab79b65240057a251af839d46d8107c59b7
Hadrian Hughes <hadrianhughes@gmail.com>
2024-03-23 13:52:11 +0000

    Get chained function calls working

--- compiler2/src/Cuttlefish/Parser/Body.hs
+++ compiler2/src/Cuttlefish/Parser/Body.hs
@@ -10,29 +10,25 @@ import Cuttlefish.Parser.Core
 import Cuttlefish.Parser.Types
 
 literalP :: Parser Expr
-literalP = FloatLit <$> float
+literalP = try (FloatLit <$> float)
        <|> IntLit   <$> int
        <|> CharLit  <$> squotes (satisfy (`notElem` ['\\', '\'']) <|> single '\\')
        <|> StrLit   <$> dquotes (takeWhileP Nothing (/= '"'))
        <|> (symbol "()" *> return UnitLit)
 
-rank1ExprP :: Parser Expr
-rank1ExprP = literalP
-          <|> VarRef    <$> identifier
-          <|> ListExpr  <$> brackets (rank2ExprP `sepBy` comma)
-          <|> TupleExpr <$> parens (rank2ExprP `sepBy` comma)
-          <|> parens rank2ExprP
+funcCallP :: Parser Expr
+funcCallP = do
+  first <- exprP
+  calls <- some (parens (exprP `sepBy` comma))
+  let chain = foldl FuncCall first calls
+  return chain
 
-rank2ExprP :: Parser Expr
-rank2ExprP = try (FuncCall <$> rank2ExprP <*> parens (rank2ExprP `sepBy` comma))
-         <|> try (StructAccess <$> rank2ExprP <*> (dot *> identifier))
-         <|> parens rank3ExprP
-
-rank3ExprP :: Parser Expr
-rank3ExprP = TernaryExpr <$> rank2ExprP <*> (symbol "?" *> rank2ExprP) <*> (symbol ":" *> rank2ExprP)
+exprP :: Parser Expr
+exprP = VarRef <$> identifier
+    <|> literalP
 
 constDefnP :: Parser ConstDefn
 constDefnP = ConstDefn
   <$> (rword "let" *> identifier)
   <*> optional (symbol ":" *> openTypeExprP)
-  <*> (symbol "=" *> rank3ExprP)
+  <*> (symbol "=" *> funcCallP)
