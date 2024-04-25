434eaf9e51630fb0b1a7959989e913ea12d8fd8a
Hadrian Hughes <hadrianhughes@gmail.com>
2024-04-25 21:42:16 +0100

    Make the Parser type an instance of Functor, Applicative and Alternative

--- compiler/src/Cuttlefish/NewParser/Core.hs
+++ compiler/src/Cuttlefish/NewParser/Core.hs
@@ -1,6 +1,33 @@
 module Cuttlefish.NewParser.Core where
 
-data Parser a = Parser { parse :: String -> Maybe (a, String) }
+import Control.Applicative
+import Data.Functor
+
+newtype Parser a = Parser { parse :: String -> Maybe (a, String) }
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input ->
+    case p input of
+      Just (x, xs) -> Just (f x, xs)
+      Nothing      -> Nothing
+
+instance Applicative Parser where
+  pure x    = Parser $ \input -> Just (x, input)
+  Parser p1 <*> Parser p2 = Parser $ \input ->
+    case p1 input of
+      Nothing          -> Nothing
+      Just (f, input') ->
+        case p2 input' of
+          Nothing           -> Nothing
+          Just (x, input'') -> Just (f x, input'')
+
+instance Alternative Parser where
+  empty = Parser $ \_ -> Nothing
+  Parser p1 <|> Parser p2 = Parser $ \input ->
+    case p1 input of
+      Nothing -> p2 input
+      result  -> result
+
 
 charP :: Char -> Parser Char
 charP c = Parser $ \input ->
