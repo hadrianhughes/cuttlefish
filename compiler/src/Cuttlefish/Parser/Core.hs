bf5aaf1cbbc8764b2cdec4c40d840ec6b422aeac
Hadrian Hughes <hadrianhughes@gmail.com>
2024-04-26 21:06:41 +0100

    Move new Parser to existing Parser module

--- compiler/src/Cuttlefish/Parser/Core.hs
+++ compiler/src/Cuttlefish/Parser/Core.hs
@@ -1,116 +1,94 @@
-module Cuttlefish.Parser.Core where
+module Cuttlefish.NewParser.Core where
 
-import           Control.Applicative (liftA2)
-import           Data.Char
-import           Text.Megaparsec
-import           Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
-import           Data.Text ( Text )
-import qualified Data.Text                  as T
-import           Control.Monad ( void )
-import           Data.Void
-import           Data.String.Conversions
-import           Cuttlefish.Parser.Ast
+import Control.Applicative
+import Data.Functor
 
-type Parser = Parsec Void Text
+newtype Parser a = Parser { parse :: String -> Maybe (a, String) }
 
-lineCmnt :: Parser ()
-lineCmnt = L.skipLineComment "//"
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input ->
+    case p input of
+      Just (x, xs) -> Just (f x, xs)
+      Nothing      -> Nothing
 
-fsc :: Parser ()
-fsc = L.space space1 lineCmnt empty
+instance Applicative Parser where
+  pure x    = Parser $ \input -> Just (x, input)
+  Parser p1 <*> Parser p2 = Parser $ \input ->
+    case p1 input of
+      Nothing          -> Nothing
+      Just (f, input') ->
+        case p2 input' of
+          Nothing           -> Nothing
+          Just (x, input'') -> Just (f x, input'')
 
-hsc :: Parser ()
-hsc = L.space hspace1 lineCmnt empty
+instance Alternative Parser where
+  empty = Parser $ \_ -> Nothing
+  Parser p1 <|> Parser p2 = Parser $ \input ->
+    case p1 input of
+      Nothing -> p2 input
+      result  -> result
 
-lexeme :: Parser a -> Parser a
-lexeme = L.lexeme hsc
+
+char :: Char -> Parser Char
+char c = Parser $ \input ->
+  case input of
+    (x:xs) | x == c -> Just (x, xs)
+    _               -> Nothing
+
+symbol :: String -> Parser String
+symbol s = sequenceA $ map charP s
+
+charIf :: (Char -> Bool) -> Parser Char
+charIf pred = Parser $ \input ->
+  case input of
+    (x:xs) | pred x -> Just (x, xs)
+    _               -> Nothing
+
+oneOf :: String -> Parser Char
+oneOf cs = charIfP (`elem` cs)
+
+charNotIn :: String -> Parser Char
+charNotIn cs = charIfP (not . (`elem` cs))
+
+lineComment :: Parser ()
+lineComment = void $ symbolP "//" *> many (charNotInP "\n")
+
+sc :: Parser ()
+sc = void $ many (lineComment <|> (void $ oneOfP " \t\n"))
+
+sc' :: Parser ()
+sc' = void $ many (lineComment <|> (void $ oneOfP " \t"))
 
 int :: Parser Int
-int = lexeme (L.signed hsc L.decimal)
+int = read <$> (some digitP)
+
+digit :: Parser Char
+digit = oneOfP "0123456789"
 
 float :: Parser Double
-float = lexeme (L.signed hsc L.float)
-
-parens :: Parser () -> Parser a -> Parser a
-parens sc = between (L.symbol sc "(") (sc *> L.symbol sc ")")
-
-brackets :: Parser () -> Parser a -> Parser a
-brackets sc = between (L.symbol sc "[") (sc *> L.symbol sc "]")
-
-braces :: Parser () -> Parser a -> Parser a
-braces sc = between (L.symbol sc "{") (sc *> L.symbol sc "}")
-
-manyInBraces :: Parser a -> Parser [a]
-manyInBraces p = braces fsc (manyTill (p <* fsc) (lookAhead $ L.symbol fsc "}"))
-
-angles :: Parser () -> Parser a -> Parser a
-angles sc = between (L.symbol sc "<") (sc *> L.symbol sc ">")
-
-squotes :: Parser a -> Parser a
-squotes = between (L.symbol hsc "'") (L.symbol hsc "'")
-
-dquotes :: Parser a -> Parser a
-dquotes = between (L.symbol hsc "\"") (L.symbol hsc "\"")
-
-comma :: Parser ()
-comma = void $ L.symbol hsc ","
-
-dot :: Parser ()
-dot = void $ L.symbol hsc "."
-
-colon :: Parser ()
-colon = void $ L.symbol hsc ":"
-
-pipe :: Parser ()
-pipe = void $ L.symbol hsc "|"
-
-rword :: Text -> Parser ()
-rword w = (lexeme . try) (string w *> notFollowedBy alphaNumChar)
-
-rws :: [Text]
-rws =
-  [ "type"
-  , "int"
-  , "float"
-  , "char"
-  , "if"
-  , "then"
-  , "else"
-  , "let"
-  , "mut"
-  , "for"
-  , "in"
-  , "of"
-  , "class"
-  , "member"
-  , "return"
-  , "match"
-  , "func"
-  , "effect" ]
-
-checkIsRW :: Text -> Parser Text
-checkIsRW w = if w `elem` rws
-  then fail $ "keyword " <> show w <> " cannot be an identifier"
-  else pure w
-
-binopChars :: [Char]
-binopChars = "&|=!><+-*/^"
-
-binop :: Parser Text
-binop = (lexeme . try) (T.pack <$> some (oneOf binopChars))
-
-typeIdentifier :: Parser Text
-typeIdentifier = (lexeme . try) p
+float = mkFloat <$> numP <*> (charP '.' *> numP)
   where
-    p = fmap T.pack $ (:) <$> upperChar
-                          <*> many alphaNumChar
+    numP = some digitP
+    mkFloat :: String -> String -> Double
+    mkFloat l r = read $ l <> "." <> r
 
-identifier :: Parser Text
-identifier = (lexeme . try) (binopP <|> normalP)
-  where
-    binopP  = parens hsc binop
-    normalP = fmap T.pack $ (:) <$> letterChar <*> many alphaNumChar
+upperChar :: Parser Char
+upperChar = oneOfP "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-sepBy2 :: Parser a -> Parser () -> Parser [a]
-sepBy2 p sep = (liftA2 (:)) (p <* sep) (p `sepBy1` sep)
+lowerChar :: Parser Char
+lowerChar = oneOfP "abcdefghijklmnopqrstuvwxyz"
+
+letterChar :: Parser Char
+letterChar = upperCharP <|> lowerCharP
+
+alphaNumChar :: Parser Char
+alphaNumChar = letterCharP <|> digitP
+
+between :: Parser open -> Parser close -> Parser a -> Parser a
+between o c p = o *> p <* c
+
+sepBy :: Parser a -> Parser sep -> Parser [a]
+sepBy p sep = sepBy' p sep <|> pure []
+
+sepBy' :: Parser a -> Parser sep -> Parser [a]
+sepBy' p sep = (:) <$> p <*> many (sep *> p)
