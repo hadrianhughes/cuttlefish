3404453ffa01d6d314353081c71da4288a30b8e4
Hadrian Hughes <hadrianhughes@gmail.com>
2024-04-27 13:36:49 +0100

    Change AST types to use String instead of Text

--- compiler/src/Cuttlefish/Parser/Ast.hs
+++ compiler/src/Cuttlefish/Parser/Ast.hs
@@ -1,77 +1,76 @@
 module Cuttlefish.Parser.Ast where
 
 import qualified Data.Map              as M
-import           Data.Text (Text)
 
 data PrimType = Int | Float | Char | Unit deriving (Show, Eq, Ord)
 
 data TypeExpr = FuncTypeExpr    TypeExpr TypeExpr
               | ListTypeExpr    TypeExpr
               | TupleTypeExpr   [TypeExpr]
-              | StructTypeExpr  (M.Map Text TypeExpr)
-              | EnumTypeExpr    (M.Map Text [TypeExpr])
+              | StructTypeExpr  (M.Map String TypeExpr)
+              | EnumTypeExpr    (M.Map String [TypeExpr])
               | EffectTypeExpr  TypeExpr
-              | PlaceholderExpr Text
+              | PlaceholderExpr String
               | PrimTypeExpr    PrimType
-              | GenericTypeExpr Text [TypeExpr]
+              | GenericTypeExpr String [TypeExpr]
               deriving (Show, Eq, Ord)
 
-data TypeConstraint = TypeConstraint { constraintClass :: Text
-                                     , constraintVar   :: Text } deriving (Show, Eq)
+data TypeConstraint = TypeConstraint { constraintClass :: String
+                                     , constraintVar   :: String } deriving (Show, Eq)
 
-data TypeDefn = TypeDefn { typeName        :: Text
+data TypeDefn = TypeDefn { typeName        :: String
                          , typeConstraints :: [TypeConstraint]
                          , typeExpr        :: TypeExpr } deriving (Show, Eq)
 
-data Expr = VarRef       Text
+data Expr = VarRef       String
           | ListAccess   Expr Expr
-          | StructAccess Expr Text
+          | StructAccess Expr String
           | IfExpr       { ifEConds :: M.Map Expr Expr, ifEElse :: Expr }
           | FuncCall     { call :: Expr, callArgs :: [Expr] }
           | EffectRun    Expr
           | ListExpr     [Expr]
           | TupleExpr    [Expr]
-          | StructExpr   (M.Map Text Expr)
+          | StructExpr   (M.Map String Expr)
           | MatchExpr    Expr (M.Map Bind Expr)
           | BlockExpr    [Statement]
           | IntLit       Int
           | CharLit      Char
-          | StrLit       Text
+          | StrLit       String
           | FloatLit     Double
           | UnitLit
           deriving (Show, Eq, Ord)
 
-data FuncDefn = FuncDefn { funcName        :: Text
+data FuncDefn = FuncDefn { funcName        :: String
                          , funcType        :: TypeExpr
                          , funcConstraints :: [TypeConstraint]
                          , funcArgs        :: [Bind]
                          , funcBody        :: Expr } deriving (Show, Eq)
 
-data ConstDefn = ConstDefn { constName  :: Text
+data ConstDefn = ConstDefn { constName  :: String
                            , constType  :: Maybe TypeExpr
                            , constValue :: Expr } deriving (Show, Eq)
 
-data ClassDefn = ClassDefn { className :: Text
-                           , classVar  :: Text
-                           , classSigs :: M.Map Text TypeExpr } deriving (Show, Eq)
+data ClassDefn = ClassDefn { className :: String
+                           , classVar  :: String
+                           , classSigs :: M.Map String TypeExpr } deriving (Show, Eq)
 
-data MembershipImpl = MembershipImpl { implName     :: Text
+data MembershipImpl = MembershipImpl { implName     :: String
                                      , implArgs     :: [Bind]
                                      , implBody     :: Expr
                                      , implIsEffect :: Bool } deriving (Show, Eq)
 
-data MembershipDefn = MembershipDefn { membClass :: Text
-                                     , membType  :: Text
+data MembershipDefn = MembershipDefn { membClass :: String
+                                     , membType  :: String
                                      , membDefns :: [MembershipImpl] } deriving (Show, Eq)
 
-data Bind = SimpleBind      Text
+data Bind = SimpleBind      String
           | TupleBind       [Bind]
-          | ConstructorBind Text [Text]
+          | ConstructorBind String [String]
           deriving (Show, Eq, Ord)
 
-data Statement = IfStmt     { ifConds :: M.Map Expr Expr
-                            , ifElse  :: Maybe Expr }
-               | VarDecl    { varName  :: Text
+data Statement = IfStmt     { ifConds  :: M.Map Expr Expr
+                            , ifElse   :: Maybe Expr }
+               | VarDecl    { varName  :: String
                             , varType  :: Maybe TypeExpr
                             , varValue :: Expr }
                | AssignStmt Expr Expr
